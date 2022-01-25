08720086a10c64e87a187b2a8662ef4a47358e8d
NaveedMohammed <rmdnaveed@gmail.com>
2022-01-25 14:03:55 -0600

    Added a NetsBlox iframe in a component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-<Home/>
+  <Home />
 </template>
 
 <script>
-import Home from './components/Home.vue'
+import Home from "./components/Home.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Home
-  }
-}
+    Home,
+  },
+};
 </script>
 
 <style>
+html,
+body {
+  min-height: 100%;
+  height: 100%;
+  margin: 0;
+}
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,27 @@
 <template>
-    <h1>Home</h1>
+  <iframe
+    src="https://physdev.c2stem.org"
+    sandbox="allow-scripts allow-same-origin"
+    height="100%"
+    width="100%"
+    frameborder="0"
+  ></iframe>
 </template>
 <script>
 export default {
-    name: 'Home',
-}
+  name: "Home",
+};
 </script>
+
+<style>
+div {
+  min-height: 100%;
+  height: 100%;
+}
+
+iframe {
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+</style>
