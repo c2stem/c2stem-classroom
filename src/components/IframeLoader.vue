e35c6c2a6f06a4854995ce79eab43b72e7a20ed9
NaveedMohammed <rmdnaveed@gmail.com>
2022-06-29 20:38:03 -0500

    Added Iframe loader component
    - This Component is a generalized c2stem build envronment loader.
    
    - Ability to load a project with username and project name in app mode, edit mode and embed mode.

---
+++ src/components/IframeLoader.vue
@@ -0,0 +1,63 @@
+<template>
+  <iframe
+    :src="iframeSource"
+    :id="iframeid"
+    sandbox="allow-scripts
+    allow-same-origin"
+    height="100%"
+    width="100%"
+    frameborder="0"
+  ></iframe>
+</template>
+
+<script>
+export default {
+  name: "IframeLoader",
+  props: {
+    source: {
+      type: String,
+      required: true,
+    },
+    iframeid: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    projectname: {
+      type: String,
+      required: true,
+    },
+    embed: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      iframeSource: "",
+    };
+  },
+  created() {
+    if (this.embed) {
+      this.iframeSource =
+        this.source +
+        "/?action=present&Username=" +
+        this.username +
+        "&ProjectName=" +
+        this.projectname +
+        "&embedMode&noExitWarning&noRun";
+    } else {
+      this.iframeSource =
+        this.source +
+        "/?action=present&Username=" +
+        this.username +
+        "&ProjectName=" +
+        this.projectname +
+        "&noExitWarning&noRun&editMode";
+    }
+  },
+};
+</script>
