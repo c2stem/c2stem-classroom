f3e15a9c02b19a5a0c560c66bda48d714d0d1a96
NaveedMohammed <rmdnaveed@gmail.com>
2022-08-02 23:53:06 -0500

    created a table component for design history

---
+++ src/components/DesignTable.vue
@@ -0,0 +1,61 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th v-for="(headerItem, index) in header" :key="index">
+          {{ headerItem }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(content, index) in contents" :key="index">
+        <td v-for="(items, i) in content" :key="i">
+          <p>{{ items }}</p>
+        </td>
+        <td>
+          <input
+            v-if="checked[index]"
+            class="form-check-input mt-0"
+            type="checkbox"
+            @change="check(content['design'], $event)"
+            checked
+          />
+          <input
+            v-else
+            class="form-check-input mt-0"
+            type="checkbox"
+            @change="check(content['design'], $event)"
+          />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "Table",
+  props: {
+    header: {
+      type: Array,
+      required: true,
+    },
+    contents: {
+      type: Object,
+      required: true,
+    },
+    checked: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    check(i, e) {
+      this.$store.dispatch("updateCheckedDesigns", {
+        index: i - 2,
+        status: e.target.checked,
+      });
+    },
+  },
+};
+</script>
