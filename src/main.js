3338ee9377b93b5123120d28e0d00e63f975ad84
NaveedMohammed <rmdnaveed@gmail.com>
2023-01-15 20:55:30 -0600

    Added eventbus to track simulation to update design history table
    - Updated run script to allow for simlation to complete

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { createApp } from "vue";
+import mitt from 'mitt'
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+const emitter = mitt();
+const app = createApp(App).use(store).use(router);
+
+app.config.globalProperties.emitter = emitter
+app.mount('#app')
 
 import "bootstrap/dist/js/bootstrap.js";
